566bf4308b4aae51bea63cd79dc48d8e38d1833e
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-06-29 11:30:16 +0300

    removed unneccessary components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,19 @@
 import Image from 'next/image';
-
-import GreenButton from '@/components/GreenButton';
+import Link from 'next/link';
 
 import './home.css';
 
 export default function Home() {
     return (
         <main className='home'>
-            <Image src='/icons/logo.svg' alt='logo' width={53} height={48} />
+            <Image id='gray-logo' src='/icons/logo.svg' alt='logo' width={43} height={39} />
             <h1>Digital Human Project</h1>
             <h5>The Only Proof of Personhood on Aptos.</h5>
             <div className='buttons'>
-                <GreenButton img='/icons/aptos.svg' text='Generate Identity' />
+                <Link id='green-button' href='/connect'>
+                    <Image src='/icons/aptos.svg' alt='aptos' width={20} height={20} />
+                    Generate Identity
+                </Link>
                 <button id='gray-button'>Connect Identity</button>
             </div>
             <div className='participants'>
