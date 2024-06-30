b36896f0d1f54d14b8fe4030176858ef324a459f
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-06-30 18:01:24 +0300

    reogranized the passport

--- src/app/digital-id/page.tsx
+++ src/app/digital-id/page.tsx
@@ -15,32 +15,37 @@ export default function DigitalHuman() {
     return (
         <main className='digital-id'>
             <Nav />
-            <Image
-                className='passport'
-                src='/images/passport-active.png'
-                alt='passport'
-                width={360}
-                height={474}
-            />
-            <h1>My Name</h1>
-            <h6 className='address'>
-                {account
-                    ? account.address.slice(0, 5) + '...' + account.address.slice(59, 64)
-                    : '0x0'}
-            </h6>
-            <div className='human-object'>
-                <Image src='/icons/aptos.svg' alt='passport' width={20} height={20} />
-                <Link href={'/'}>View Digital Human ID</Link>
-            </div>
-            <h6>Biometrics</h6>
-            <div className='biometrics'>
-                <BiometricsMethod name='Iris' image='iris.svg' href='/data/iris' />
-                <BiometricsMethod
-                    name='Fingerprint'
-                    image='fingerprint.svg'
-                    href='/data/fingerprint'
+            <section>
+                <Image
+                    className='passport'
+                    src='/images/passport-active.png'
+                    alt='passport'
+                    width={360}
+                    height={474}
                 />
-            </div>
+                <h1>My Name</h1>
+                <h6 className='address'>
+                    {account
+                        ? account.address.slice(0, 5) + '...' + account.address.slice(59, 64)
+                        : '0x0'}
+                </h6>
+                <div className='human-object'>
+                    <Image src='/icons/aptos.svg' alt='passport' width={20} height={20} />
+                    <Link href={'/'}>View Digital Human ID</Link>
+                </div>
+                <h6>Biometrics</h6>
+                <div className='biometrics'>
+                    <BiometricsMethod name='Iris' image='iris.svg' href='/data/iris' />
+                    <BiometricsMethod
+                        name='Fingerprint'
+                        image='fingerprint.svg'
+                        href='/data/fingerprint'
+                    />
+                </div>
+                <Link href={'/'} className='home-button'>
+                    <Image src='/icons/home-button.svg' alt='home' width={72} height={72} />
+                </Link>
+            </section>
         </main>
     );
 }
