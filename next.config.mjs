9af79a66cc18f63ab1b79b5f66b71cb1e3edd8a0
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-07-07 23:55:15 +0300

    redeployed the contract

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        NEXT_PUBLIC_MODULE: 'e73532dd6f4220d8211caa8fa472d331ff973cac272222a8769a03f06a62b9c2',
+    },
+};
 
 export default nextConfig;
