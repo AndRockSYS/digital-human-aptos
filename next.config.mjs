01752a0407f60f89630eca8b44f5474110889d6a
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-07-09 10:39:33 +0300

    updates

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,8 @@ const nextConfig = {
     env: {
         NEXT_PUBLIC_MODULE: '4179a61785b7ea89fad11d45b578841fbc00063a8f1b833d47f077965056009b',
 
-        PINATA_API: '9bec6441c57b5f2f8da0',
-        PINATA_SECRET: 'ef0270cf8498ea621ea9bf1cbe89419fa2c2819f9382c3b24bddbb66835f0b54',
+        PINATA_API: '0a7afcd01ae3603e96a9',
+        PINATA_SECRET: '1610c124fb664324d1c9d1fa0fccf15af524e533df2916aef8eb59e99c83b453',
         PINATA_URL: 'https://rose-principal-turtle-588.mypinata.cloud/ipfs/',
 
         ID_IMAGE: 'QmXW4ojVRb7QxzSZJp8qGRsNqJXxHvQGQDfDR43KsGS1RJ',
