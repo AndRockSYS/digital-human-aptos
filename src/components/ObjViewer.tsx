dd8726042decd747b5e4caac297dd38186a38223
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-07-10 18:45:32 +0300

    added react native webview

--- src/components/ObjViewer.tsx
+++ src/components/ObjViewer.tsx
@@ -1,7 +1,21 @@
-import { useLoader } from '@react-three/fiber';
-import { OBJLoader } from 'three/examples/jsm/Addons.js';
+'use client';
 
-export default function ObjViewer({ file }: { file: File }) {
-    // const objFile = useLoader(OBJLoader, file);
-    return <>{/* <primitive object={objFile} /> */}</>;
+import { useRef } from 'react';
+
+import { View } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+export default function ObjViewer({ objLink }: { objLink: string }) {
+    const webViewRef = useRef<WebView | null>(null);
+
+    return (
+        <View style={{ flex: 1, backgroundColor: '#fff' }}>
+            <View>
+                <WebView
+                    ref={webViewRef}
+                    source={{ uri: `https://3dviewer.net/index.html#model=${objLink}` }}
+                />
+            </View>
+        </View>
+    );
 }
