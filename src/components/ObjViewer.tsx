4bf271f456f16b7a4feb0c1f5120a15e9ac682a0
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-07-10 12:40:16 +0300

    remade obj viewer

--- src/components/ObjViewer.tsx
+++ src/components/ObjViewer.tsx
@@ -6,55 +6,38 @@ export default function ObjViewer({ file }: { file: File }) {
     const viewerRef = useRef<OV.EmbeddedViewer | null>(null);
 
     useEffect(() => {
-        // Set the location of the libraries needed to load different models to lib, which in nextjs will point to "/public/libs"
-        OV.SetExternalLibLocation('libs');
-        //OV.Init3DViewerElements(() => {});
-        if (!viewerRef.current) {
-            const viewer = new OV.EmbeddedViewer(parentDiv.current as HTMLElement, {
+        window.addEventListener('load', () => {
+            if (!parentDiv.current) return;
+            OV.SetExternalLibLocation('libs');
+
+            let viewer = new OV.EmbeddedViewer(parentDiv.current, {
                 camera: new OV.Camera(
-                    new OV.Coord3D(-150.0, 200.0, 300.0),
+                    new OV.Coord3D(-1.5, 2.0, 3.0),
                     new OV.Coord3D(0.0, 0.0, 0.0),
                     new OV.Coord3D(0.0, 1.0, 0.0),
                     45.0
                 ),
-                backgroundColor: new OV.RGBAColor(100, 100, 100, 255),
-                defaultColor: new OV.RGBColor(0, 100, 100),
-                onModelLoaded: () => {
-                    // @ts-ignore
-                    console.log(viewerRef.current.GetViewer());
-                },
+                backgroundColor: new OV.RGBAColor(255, 255, 255, 255),
+                defaultColor: new OV.RGBColor(200, 200, 200),
+                edgeSettings: new OV.EdgeSettings(false, new OV.RGBColor(0, 0, 0), 1),
+                environmentSettings: new OV.EnvironmentSettings(
+                    [
+                        'assets/envmaps/fishermans_bastion/posx.jpg',
+                        'assets/envmaps/fishermans_bastion/negx.jpg',
+                        'assets/envmaps/fishermans_bastion/posy.jpg',
+                        'assets/envmaps/fishermans_bastion/negy.jpg',
+                        'assets/envmaps/fishermans_bastion/posz.jpg',
+                        'assets/envmaps/fishermans_bastion/negz.jpg',
+                    ],
+                    false
+                ),
             });
 
-            viewer.Resize = () => {
-                console.log("I'm not resizing");
-            };
-            viewerRef.current = viewer;
             viewer.LoadModelFromFileList([file]);
-        }
+        });
 
-        // return () => {
-        //     if (viewerRef.current && parentDiv.current) {
-        //         delete viewerRef.current.model;
-        //         viewerRef.current.viewer.renderer.resetState();
-        //         viewerRef.current.viewer.Clear();
-        //         // @ts-ignore
-        //         delete viewerRef.current.viewer;
-        //         const gl = viewerRef.current.canvas.getContext('webgl2');
-        //         // @ts-ignore
-        //         gl.getExtension('WEBGL_lose_context').loseContext();
-
-        //         const tempClone = viewerRef.current.canvas.cloneNode(true);
-        //         // @ts-ignore
-        //         viewerRef.current.canvas.parentNode.replaceChild(
-        //             tempClone,
-        //             viewerRef.current.canvas
-        //         );
-
-        //         parentDiv.current.removeChild(parentDiv.current.children[0]);
-        //         viewerRef.current = null;
-        //     }
-        // };
-    }, [file]);
+        return window.removeEventListener('load', () => {});
+    }, [parentDiv]);
 
     return (
         <>
