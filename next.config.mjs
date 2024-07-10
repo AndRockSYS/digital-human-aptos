9286cc4ff1d791e9f6aab426d8d8489b85cef182
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-07-10 18:35:59 +0300

    redeployed the contract

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        NEXT_PUBLIC_MODULE: 'c35617f2deeaeeb8c09b6c578154e64ae34329718b879598e4d28a24df569f0b',
+        NEXT_PUBLIC_MODULE: '9025db89c6f67e6841d55d91a99db450b278408de9357a9fe14834a17fcc0b3e',
 
         PINATA_API: '0a7afcd01ae3603e96a9',
         PINATA_SECRET: '1610c124fb664324d1c9d1fa0fccf15af524e533df2916aef8eb59e99c83b453',
